d8d1a5a98a4617950f9e0ebdb655dcc879ea71d8
Hassam-Ata <hassamata86@gmail.com>
2026-06-14 20:17:36 +0500

    configured axios and react query

---
+++ apps/mobile/src/app/index.tsx
@@ -0,0 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+import { HealthCheckResponse } from "@food-delivery/types";
+import { api } from "../lib/axios";
+import { ActivityIndicator, Text, View } from "react-native";
+
+export default function HomeScreen() {
+  const {
+    data: health,
+    error,
+    isLoading,
+  } = useQuery<HealthCheckResponse>({
+    queryKey: ["health"],
+    queryFn: () =>
+      api.get<HealthCheckResponse>("/health").then((res) => res.data),
+  });
+  console.log("API URL:", process.env.EXPO_PUBLIC_API_URL);
+  return (
+    <View>
+      <Text>Food Delivery</Text>
+      <Text>Connection Text</Text>
+      {isLoading && <ActivityIndicator size="large" color="#ff6b35" />}
+
+      {health && (
+        <View>
+          <Text>Api Status:{health.status}</Text>
+          <Text>{new Date(health.timestamp).toLocaleTimeString()}</Text>
+        </View>
+      )}
+      {error && (
+        <View>
+          <Text>Cannot reach the api</Text>
+        </View>
+      )}
+    </View>
+  );
+}

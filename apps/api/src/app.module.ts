96e990add0246142ee9ed1d7c6fe535831e2d068
Hassam-Ata <hassamata86@gmail.com>
2026-06-15 23:08:48 +0500

    added auth decorators

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './db/db.module';
+import { AuthService } from './auth/auth.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), DbModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('/api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   const PORT = process.env.PORT ?? 3000;
   await app.listen(PORT);
   console.log(`API running on port ${PORT}`);
